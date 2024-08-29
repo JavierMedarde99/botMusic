448fc46c6851121c71375099cc919718e49501d2
JavierMedardeMata <32664879+JavierMedarde99@users.noreply.github.com>
2024-08-29 18:12:26 +0200

    connect with spotify

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
+from urllib import request
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy
 import os
 
 load_dotenv()
 
 token = os.getenv('DISCORD_TOKEN')
+client_spotify = os.getenv('SPOTIFY_CLIENT')
+secret_spotify = os.getenv('SPOTIFY_SECRET')
 
 intents = discord.Intents.all()
 intents.messages = True
@@ -13,14 +18,24 @@ intents.members = True
 
 bot = commands.Bot(command_prefix="!",intents=intents)
 
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_spotify, client_secret=secret_spotify))
+
 @bot.command()
 async def info(ctx):
     await ctx.send("hola mundo")
 
 @bot.command()
+async def test(ctx):
+    track = sp.playlist('37i9dQZF1DX1HCSfq0nSal')
+    print(track["tracks"]["items"][0]["track"]["external_urls"]["spotify"])
+
+@bot.command()
 async def play(ctx):
     channel = ctx.message.author.voice.channel
-    await channel.connect()
+    connect = await channel.connect()
+    url = "spotify:track:0zpIdU4Q65JlzyueQQnx7Q"
+    audio = discord.FFmpegPCMAudio(url,executable="ffmpeg")
+    await connect.play(audio)
 
 @bot.event 
 async def on_message(message):
